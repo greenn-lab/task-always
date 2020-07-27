e4523b724297e6657c060852d1409535b88a3c61
greenn-lab <greenn.lab@gmail.com>
2020-07-27 23:51:48 +0900

    initial commit

---
+++ api-server/src/test/java/study/spring/taskalways/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.taskalways.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
