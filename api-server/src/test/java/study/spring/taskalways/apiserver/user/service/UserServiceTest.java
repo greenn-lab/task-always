b1b4e51be207aa7034966294d2cd9844645fde35
greenn-lab <greenn.lab@gmail.com>
2020-07-29 23:07:28 +0900

    add//test sign-up api

---
+++ api-server/src/test/java/study/spring/taskalways/apiserver/user/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package study.spring.taskalways.apiserver.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import study.spring.taskalways.apiserver.user.domain.dto.SignUpRequest;
+import study.spring.taskalways.apiserver.user.repository.UserRepository;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class UserServiceTest {
+
+  ;
+
+  @Test
+  void shouldSignUpUser() {
+    // GIVEN
+    final UserRepository repository = mock(UserRepository.class);
+    final UserService service = new UserService(repository);
+
+    // WHEN
+    final SignUpRequest req = new SignUpRequest();
+    req.setNickname("tester");
+    req.setEmail("tester@test.com");
+    req.setPin("Test123!");
+
+    service.signUp(req);
+
+    // THEN
+    verify(repository).save(req.toUser());
+  }
+
+}
